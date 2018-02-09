9829221126c4f037a0db9503ade13b9c36e16230
Zhang Lishun <zhanglishun@le.com>
2018-02-09 12:02:10 +0800

    重构了一下，尽量减少类的使用，更易理解dl

--- app/src/main/kotlin/com/fashare/app/App.kt
+++ app/src/main/kotlin/com/fashare/app/App.kt
@@ -3,23 +3,16 @@ package com.fashare.app
 import android.app.Application
 import android.net.Uri
 import android.os.Environment
-import com.fashare.app.util.FileUtil
 import com.fashare.dl.DL
-import com.fashare.dl.extension.loge
 import java.io.File
 
 class App: Application(){
     override fun onCreate() {
         super.onCreate()
+
         DL.replaceInstrumentation()
-
         val pluginFile = File(Environment.getExternalStorageDirectory(), "testapk-with-res.apk")
-        try {
-            FileUtil.writeBytesToFile(assets.open("testapk-with-res.apk"), pluginFile)
-        }catch (e: Exception){
-            loge(e)
-        }
-
+        assets.open("testapk-with-res.apk").copyTo(pluginFile.outputStream())
         DL.loadApk(this, Uri.fromFile(pluginFile))
     }
 }
--- dl/src/main/kotlin/com/fashare/dl/DL.kt
+++ dl/src/main/kotlin/com/fashare/dl/DL.kt
@@ -3,15 +3,36 @@ package com.fashare.dl
 import android.app.Instrumentation
 import android.content.Context
 import android.net.Uri
+import android.util.Log
+import android.widget.Toast
 import dalvik.system.DexClassLoader
 import org.joor.Reflect
+
+/**
+ * <pre>
+ *     author : jinliangshan
+ *     e-mail : jinliangshan@chexiang.com
+ *     desc   :
+ * </pre>
+ */
+fun Context.toast(msg: String?){
+    Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+}
+
+fun Any.logd(msg: Any?){
+    Log.d(this.javaClass.simpleName, msg.toString())
+}
+
+fun Any.loge(msg: Any?){
+    Log.e(this.javaClass.simpleName, msg.toString())
+}
+
 
 /**
  * 动态加载（门面类）
  */
 object DL {
     lateinit var dexClassLoader: DexClassLoader
-    internal lateinit var instrumentation: Instrumentation
 
     /**
      * 加载 sdcard 上的 未安装的 apk
@@ -28,11 +49,12 @@ object DL {
      * 替换 ActivityThread.mInstrumentation
      */
     fun replaceInstrumentation() {
-        Reflect.on("android.app.ActivityThread").call("currentActivityThread").apply {
-            val activityThreadRef = this
-            val base = activityThreadRef.get<Instrumentation>("mInstrumentation")
-            instrumentation = InstrumentationProxy(base)
-            activityThreadRef.set("mInstrumentation", instrumentation)
+        Reflect.on("android.app.ActivityThread")
+                .call("currentActivityThread").apply {
+
+            val base = get<Instrumentation>("mInstrumentation")
+            val instrumentation = InstrumentationProxy(base)
+            set("mInstrumentation", instrumentation)
         }
     }
 }
